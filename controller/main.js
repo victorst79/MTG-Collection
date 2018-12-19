1e911f338fd5d2090e670ce98f43033203ec2dc1
Victor Santaella <victorst79@gmail.com>
2018-12-19 12:53:41 +0100

    add boostrap and jquery

--- controller/main.js
+++ controller/main.js
@@ -1,10 +1,24 @@
 $("document").ready(function(){
+    var page = 1;
+    var $cards = $("section#mtg-cards");
     console.log("DOCUMENT LOAD");
 
     $.ajax({
-        url: "https://api.magicthegathering.io/v1/cards",
-        succes: function(){
-            console.log(cards);
-        }
-    });
+        method: "GET",
+        url: "https://api.magicthegathering.io/v1/cards?page="+ page +"&pageSize=12"
+    })
+        .done(function(result){
+            console.log(result);
+
+            $cards.append("<div class='mtg-card'></div>");
+            var $mtg_card = $("div.mtg-card");
+            // TAGS
+            $mtg_card.append("<h3>"+ result.cards[0].name +"</h3>");
+            $mtg_card.append("<img src="+ result.cards[0].imageUrl +">")
+
+        })
+
+        .fail(function(){
+            console.error("API NOT WORKING");
+        });
 });
