1e2699eab7e56bd5b83710aa9cbf0fe10cc0849d
unknown <victorst79@gmail.com>
2018-12-20 14:36:17 +0100

    loop added for estructural information of cards

--- controller/main.js
+++ controller/main.js
@@ -8,13 +8,17 @@ $("document").ready(function(){
         url: "https://api.magicthegathering.io/v1/cards?page="+ page +"&pageSize=12"
     })
         .done(function(result){
-            console.log(result);
+            console.log(result.cards.length);
 
-            $cards.append("<div class='mtg-card'></div>");
-            var $mtg_card = $("div.mtg-card");
-            // TAGS
-            $mtg_card.append("<h3>"+ result.cards[0].name +"</h3>");
-            $mtg_card.append("<img src="+ result.cards[0].imageUrl +">");
+            
+            for(let i = 0; i < result.cards.length; i++){
+                $cards.append("<div class='mtg-card'></div>");
+                var $mtg_card = $("div.mtg-card");
+                
+                // TAGS
+                $mtg_card.append("<h3>"+ result.cards[i].name +"</h3>");
+                $mtg_card.append("<img src="+ result.cards[i].imageUrl +">");
+            }
         })
 
         .fail(function(){
