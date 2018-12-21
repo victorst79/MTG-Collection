7ca5912ab0c43422edc39f23fcf20b6eaa5bbc70
unknown <victorst79@gmail.com>
2018-12-21 12:35:24 +0100

    jquery working /w boostrap, need fix scroll

--- controller/main.js
+++ controller/main.js
@@ -8,16 +8,14 @@ $("document").ready(function(){
         url: "https://api.magicthegathering.io/v1/cards?page="+ page +"&pageSize=12"
     })
         .done(function(result){
-            console.log(result.cards.length);
-
+            console.log(result.cards.length);            
             
             for(let i = 0; i < result.cards.length; i++){
-                $cards.append("<div class='mtg-card'></div>");
-                var $mtg_card = $("div.mtg-card");
-                
+                $cards.append("<div class='mtg-card col-md-4'></div>");
+                var $card = $("div.mtg-card:last-child");
                 // TAGS
-                $mtg_card.append("<h3>"+ result.cards[i].name +"</h3>");
-                $mtg_card.append("<img src="+ result.cards[i].imageUrl +">");
+                $card.append("<h3>"+ result.cards[i].name +"</h3>");
+                $card.append("<img src="+ result.cards[i].imageUrl +">");
             }
         })
 
