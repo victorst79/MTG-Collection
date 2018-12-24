9418c6952cecdcae0f6282f9d702322d867d42e8
unknown <victorst79@gmail.com>
2018-12-24 16:08:23 +0100

    search function add

--- controller/main.js
+++ controller/main.js
@@ -23,25 +23,30 @@ $("document").ready(function(){
         .fail(function(){
             console.error("API NOT WORKING");
         });
-    
-        // SCROLL FUNCTION
-	    $win.scroll( function() {
-            if ( $(document).height() - $win.height() == $win.scrollTop()) {
-                $.ajax({
-                    method: "GET",
-                    url: "https://api.magicthegathering.io/v1/cards?page="+ page +"&pageSize=12",
-                    success: function(result) {
+
+    // SCROLL FUNCTION
+	$win.scroll( function() {
+        if ( $(document).height() - $win.height() == $win.scrollTop()) {
+            $.ajax({
+                method: "GET",
+                url: "https://api.magicthegathering.io/v1/cards?page="+ page +"&pageSize=12",
+                success: function(result) {
                         
-                        for(let i = 0; i < result.cards.length; i++){
-                            $cards.append("<div class='mtg-card col-md-4'></div>");
-                            var $card = $("div.mtg-card:last-child");
-                            // TAGS
-                            $card.append("<h3>"+ result.cards[i].name +"</h3>");
-                            $card.append("<img src="+ result.cards[i].imageUrl +">");
-                        }
-                        page++;
+                    for(let i = 0; i < result.cards.length; i++){
+                        $cards.append("<div class='mtg-card col-md-4'></div>");
+                        var $card = $("div.mtg-card:last-child");
+                        // TAGS
+                        $card.append("<h3>"+ result.cards[i].name +"</h3>");
+                        $card.append("<img src="+ result.cards[i].imageUrl +">");
                     }
+                    page++;
+                }
             });
 		}
-	});
+    });
+    
+    // SEARCH FUNCTION
+    $("button#search").click(function(){
+        console.log("entra");
+    });
 });
